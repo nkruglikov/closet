0df3bc06f43e72449bad538dac05a4524a07e590
Nikolai Kruglikov <kkrugl@yandex.ru>
2015-10-03 16:34:43 +0300

    Draw full cube

--- test.js
+++ test.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
     var ctx = canvas.getContext("2d");
     var engine = new Engine(ctx);
 
-    engine.draw(new PolygonXY(new Point(0, 0, 0), 10, 10));
+    var pA = new Point(10, 10, 10),
+        pB = new Point(20, 20, 20);
+    engine.draw(new PolygonXY(pA, 10, 10));
+    engine.draw(new PolygonXY(pB, -10, -10));
+    engine.draw(new PolygonXZ(pA, 10, 10));
+    engine.draw(new PolygonXZ(pB, -10, -10));
+    engine.draw(new PolygonYZ(pA, 10, 10));
+    engine.draw(new PolygonYZ(pB, -10, -10));
 });
