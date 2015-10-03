a49851bdc04730ce533cc121ca9bdcb24cdfdc9e
Nikolai Kruglikov <kkrugl@yandex.ru>
2015-10-04 01:17:10 +0300

    The comparator now defines comparsion relation

--- scene.js
+++ scene.js
@@ -21,45 +21,19 @@ Scene.prototype.redraw = function() {
     })(this.models);
 
     function polygonComparator(p1, p2) {
-        // Same types
-        if (p1 instanceof PolygonXY && p2 instanceof PolygonXY)
-            return p2.pointA.z - p1.pointA.z;
-        else if (p1 instanceof PolygonXZ && p2 instanceof PolygonXZ)
-            return p1.pointA.y - p2.pointA.y;
-        else if (p1 instanceof PolygonYZ && p2 instanceof PolygonYZ)
-            return p1.pointA.x - p2.pointA.x;
-        // Different types
-        else if (p1 instanceof PolygonXY && p2 instanceof PolygonYZ) {
-            var x0 = p1.minX() - p2.minX();
-            var z0 = p1.minZ() - p2.minZ();
-            var y0 = p1.minY() - p2.minY();
-            var y1 = p2.maxY() - p2.minY();
-            return x0 >= 0 || z0 <= 0 || y0 >= y1 ? 1 : -1;
-        }
-        else if (p1 instanceof PolygonXY && p2 instanceof PolygonXZ) {
-            var y0 = p1.minY() - p2.minY();
-            var z0 = p1.minZ() - p2.minZ();
-            var x0 = p1.minX() - p2.minX();
-            var x1 = p2.maxX() - p2.minX();
-            return y0 >= 0 || z0 <= 0 || x0 >= x1 ? 1 : -1;
-        }
-        else if (p1 instanceof PolygonXZ && p2 instanceof PolygonYZ) {
-            var x0 = p1.minX() - p2.minX();
-            var z2 = p1.maxZ() - p2.minZ();
-            var y0 = p1.minY() - p2.minY();
-            var y1 = p2.maxY() - p2.minY();
-            return x0 >= 0 || z2 <= 0 || y0 >= y1 ? 1 : -1;
-        }
-        // Different types in another order
-        else if (p2 instanceof PolygonXY && p1 instanceof PolygonYZ ||
-                 p2 instanceof PolygonXY && p1 instanceof PolygonXZ ||
-                 p2 instanceof PolygonXZ && p1 instanceof PolygonYZ)
-            return -polygonComparator(p2, p1);
+        return  ( p1.maxZ() < p2.minZ() ?  1
+                : p1.minZ() > p2.maxZ() ? -1
+                : p1.minX() > p2.maxX() ?  1
+                : p1.maxX() < p2.minX() ? -1
+                : p1.minY() > p2.maxY() ?  1
+                : p1.maxY() < p2.minY() ? -1
+                : 0);
     }
 
     polygons.sort(polygonComparator);
 
     for (var i = 0; i < polygons.length; i++) {
         this.engine.draw(polygons[i]);
+        console.log(polygons[i].constructor.name, polygons[i].fillStyle);
     }
 }
--- test.js
+++ test.js
@@ -14,17 +14,17 @@ $(document).ready(function() {
     engine.draw(new PolygonYZ(pB, -10, -10, fill));
 
     var scene = new Scene(engine);
-    pXY1 = new PolygonXY(new Point(1, 1, 0), 5, 5);
-    pXY2 = new PolygonXY(new Point(1, 1, 4), 5, 5);
-    pXY3 = new PolygonXY(new Point(1, 1, 2), 5, 5);
+    pXY1 = new PolygonXY(new Point(1, 1, 0), 5, 5, "salmon");
+    pXY2 = new PolygonXY(new Point(1, 1, 4), 5, 5, "lightgreen");
+    pXY3 = new PolygonXY(new Point(1, 1, 2), 5, 5, "lightblue");
     scene.models.push(pXY1, pXY2, pXY3);
-    pXZ1 = new PolygonXZ(new Point(10, 1, 0), 5, 5);
-    pXZ2 = new PolygonXZ(new Point(10, 3, 0), 5, 5);
-    pXZ3 = new PolygonXZ(new Point(10, 2, 0), 5, 5);
+    pXZ1 = new PolygonXZ(new Point(10, 1, 0), 5, 5, "salmon");
+    pXZ2 = new PolygonXZ(new Point(10, 3, 0), 5, 5, "lightgreen");
+    pXZ3 = new PolygonXZ(new Point(10, 2, 0), 5, 5, "lightblue");
     scene.models.push(pXZ1, pXZ2, pXZ3);
-    pYZ1 = new PolygonYZ(new Point(20, 1, 0), 5, 5);
-    pYZ2 = new PolygonYZ(new Point(22, 1, 0), 5, 5);
-    pYZ3 = new PolygonYZ(new Point(21, 1, 0), 5, 5);
+    pYZ1 = new PolygonYZ(new Point(20, 1, 0), 5, 5, "salmon");
+    pYZ2 = new PolygonYZ(new Point(22, 1, 0), 5, 5, "lightgreen");
+    pYZ3 = new PolygonYZ(new Point(21, 1, 0), 5, 5, "lightblue");
     scene.models.push(pYZ1, pYZ2, pYZ3);
     pXY4 = new PolygonXY(new Point(25, 0, 10), 15, 15, "yellow");
     pXZ4 = new PolygonXZ(new Point(30, 1, 0), 5, 5, "green");
