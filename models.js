ffa7fb9e982c82301eeadab35307441a8b1c4c3f
Nikolai Kruglikov <kkrugl@yandex.ru>
2015-10-04 13:28:51 +0300

    Add logs models

--- models.js
+++ models.js
@@ -2,13 +2,53 @@ function Model(models) {
     this.models = models;
 }
 
+
 Block.prototype = new Model();
 Block.prototype.constructor = Block;
 function Block(point, width, height, depth, color) {
+    if (arguments.length >= 4)
+        this.init(point, width, height, depth, color);
+}
+
+Block.prototype.init = function (point, width, height, depth, color) {
     this.polygonXY = new PolygonXY(point, width, height, color);
     this.polygonXZ = new PolygonXZ(new Point(point.x,
                 point.y + height, point.z), width, depth, color);
     this.polygonYZ = new PolygonYZ(new Point(point.x + width,
                 point.y, point.z), height, depth, color);
     this.models = [this.polygonXY, this.polygonXZ, this.polygonYZ];
+}
+
+Log.prototype = new Block();
+Log.prototype.constructor = Log;
+function Log() {
+    this.thickness = Log.thickness;
+}
+Log.thickness = 0.5;
+
+
+LogXY.prototype = new Log();
+LogXY.prototype.constructor = LogXY;
+function LogXY(point, width, height, color, texture) {
+    texture = texture || color;
+    this.init(point, width, height, this.thickness, color);
+    this.polygonXY.fillStyle = texture;
+}
+
+
+LogXZ.prototype = new Log();
+LogXZ.prototype.constructor = LogXZ;
+function LogXZ(point, width, depth, color, texture) {
+    texture = texture || color;
+    this.init(point, width, this.thickness, depth, color);
+    this.polygonXZ.fillStyle = texture;
+}
+
+
+LogYZ.prototype = new Log();
+LogYZ.prototype.constructor = LogYZ;
+function LogYZ(point, height, depth, color, texture) {
+    texture = texture || color;
+    this.init(point, this.thickness, height, depth, color);
+    this.polygonYZ.fillStyle = texture;
 }
